741335ebcf8ff85f1eb4b574b59ffb4d36696819
Савицкий Даня <savitskiydanulo@gmail.com>
2020-11-27 00:38:00 +0200

    Создал тригер для перетаскивания окна по левой кнопке мыши.

---
+++ Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,26 @@
+﻿using ScheduleGenerator.Infrastructure.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleGenerator.Infrastructure.Commands
+{
+    class LambdaCommand : Command
+    {
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+
+        public LambdaCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _Execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _CanExecute = canExecute;
+        }
+
+
+        public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => _Execute(parameter);
+    }
+}
--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,19 +1,37 @@
-﻿using ScheduleGenerator.ViewModels.Base;
+﻿using ScheduleGenerator.Infrastructure.Commands;
+using ScheduleGenerator.ViewModels.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace ScheduleGenerator.ViewModels
 {
     class MainViewModel : ViewModel
     {
+        #region Свойства
         private string _Title = "Some title";
         public string Title
         {
             get => _Title;
             set => Set(ref _Title, value);
+        }
+        #endregion
+
+        #region Команды
+        public ICommand DragMoveWindowCommand { get; }
+        private bool CanDragMoveWindowExecute(object parameter) => true;
+        private void OnDragMoveWindowExecuted(object parameter) => (parameter as Window).DragMove();
+        #endregion
+
+        public MainViewModel()
+        {
+            #region Инициализация команд
+            DragMoveWindowCommand = new LambdaCommand(OnDragMoveWindowExecuted, CanDragMoveWindowExecute);
+            #endregion
         }
     }
 }
