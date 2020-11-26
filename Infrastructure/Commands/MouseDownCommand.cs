9d486420069b6d6d738777d3fb3972b2cbc10a47
Савицкий Даня <savitskiydanulo@gmail.com>
2020-11-26 22:52:43 +0200

    Установил - Behaviours.Wpf Интерфейс.

---
+++ Infrastructure/Commands/MouseDownCommand.cs
@@ -0,0 +1,21 @@
+﻿using ScheduleGenerator.Infrastructure.Commands.Base;
+using System;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace ScheduleGenerator.Infrastructure.Commands
+{
+    class MouseDownCommand : Command
+    {
+        public override bool CanExecute(object parameter) => true;
+        public override void Execute(object parameter) => (parameter as Window).DragMove();
+    }
+
+    //class MouseDownCommandExtension : MarkupExtension
+    //{
+    //    public override object ProvideValue(IServiceProvider serviceProvider)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
+}
