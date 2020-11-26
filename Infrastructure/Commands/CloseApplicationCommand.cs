5c972bcafbd6de742025f14d460a47d1349fc7bd
Савицкий Даня <savitskiydanulo@gmail.com>
2020-11-26 21:09:54 +0200

    Создал и прикрепил команду закрытия программы. CloseApplicationCommand.cs

---
+++ Infrastructure/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,16 @@
+﻿using ScheduleGenerator.Infrastructure.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleGenerator.Infrastructure.Commands
+{
+    class CloseApplicationCommand : Command
+    {
+        public override bool CanExecute(object parameter) => true;
+
+        public override void Execute(object parameter) => App.Current.Shutdown();
+    }
+}
