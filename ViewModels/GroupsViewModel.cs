aeeab678e4b459adf77a050c8bd3697632b17d38
Савицкий Даня <savitskiydanulo@gmail.com>
2020-11-27 15:14:37 +0200

    Навигация по страницам работает хорошо. Добавил модели-представления.

--- ViewModels/GroupsViewModel.cs
+++ ViewModels/GroupsViewModel.cs
@@ -4,5 +4,15 @@ namespace ScheduleGenerator.ViewModels
 {
     class GroupsViewModel : ViewModel
     {
+        #region Свойства
+        #region Текст
+        private string _Title = "Groups";
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+        #endregion
+        #endregion
     }
 }
--- ViewModels/TeachersViewModel.cs
+++ ViewModels/TeachersViewModel.cs
@@ -4,5 +4,15 @@ namespace ScheduleGenerator.ViewModels
 {
     class TeachersViewModel : ViewModel
     {
+        #region Свойства
+        #region Текст
+        private string _Title = "Teachers";
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+        #endregion
+        #endregion
     }
 }
